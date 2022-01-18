801e0b6d18a582f004521d3c9c9797886663739d
Maxime_MASSONNEAU <mxe.massonneau@free.fr>
2022-01-18 10:35:30 +0100

    ajout articles 18/01/2022 1.3

--- cv.js
+++ cv.js
@@ -2,36 +2,34 @@
 const ThemeMenu_BTN_ = document.getElementById("ThemeMenu_BTN");//======================================BTN-ThemeMenu
 const Competances_glass_ = document.getElementById('Competances_glass');
 const diplome_ = document.getElementById("diplome");
-let coefMOZ = 80; let coefMOZI = 0; let innerWIDTH = window.innerWidth; let innerHEIGHT = innerHeight;
+let coefMOZ_START = 80; let coefMOZI_PiX = 0; 
 //document.addEventListener("scroll", () => {  console.log( "scrollY : " + scrollY ); })
 //---------------------------------------------------------------------------------------------------------------------Fonction utiles
-if( window.navigator.vendor == "Google Inc."){ coefMOZ = 70; coefMOZI = 5 }else{ coefMOZ=60; coefMOZI= 15 }
-let Competan_pos_TOP = {0:-20, 1:-parseInt(coefMOZ +"00"), 2:-170, 3:-125, 4:-80, 5:-40, 5:-15, };
+if( window.navigator.vendor == "Google Inc."){ coefMOZ_START = 70; coefMOZI_PiX = 5 }else{ coefMOZ_START=60; coefMOZI_PiX= 15 }
+let Competan_pos_TOP = {0:-20, 1:-parseInt(coefMOZ_START +"00"), 2:-170, 3:-125, 4:-80, 5:-40, 5:-15, };
 let Bool_Compet_opn = false;
 
 const slid_Compet_OPN = () => {  
   document.getElementById("Competances").style.setProperty("--opn_cptce", "opn_cptce");
-  document.getElementById("Competances_glass").style.setProperty("--opn_glass_LEFT", "50%");
-  document.getElementById("Competances_glass").style.setProperty("--opn_glass", "opn_glass");
-  if(window.innerWidth > 955){
-    document.getElementById("exper").style.setProperty("--LONG_exper", "SHORT_exper");
-  }  
+  document.getElementById("Competances_glass").classList = "glass_OPN";
+  if(window.innerWidth > 955){ document.getElementById("exper").classList = "exper_SHORT";}  
   Bool_Compet_opn = true; 
-  rebond_("visi_card",Competan_pos_TOP,5+coefMOZI,0);    
+  rebond_("visi_card",Competan_pos_TOP,5+coefMOZI_PiX,0);    
 }
 const slid_Compet_CLS = () => {
-  document.getElementById("Competances").style.setProperty("--opn_cptce", "cls_cptce");
-  document.getElementById("Competances_glass").style.setProperty("--opn_glass_LEFT", "-140%");
-  document.getElementById("Competances_glass").style.setProperty("--opn_glass", "cls_glass");  
-  document.getElementById("exper").style.setProperty("--LONG_exper", "LONG_exper");
-
+  document.getElementById("Competances").style.setProperty("--opn_cptce", "cls_cptce");  
+  document.getElementById("Competances_glass").classList = "glass_CLS"; 
+  document.getElementById("exper").classList = "exper_LONG";
   Bool_Compet_opn = false;
+  document.querySelectorAll("#langue_fr_BAR, #langue_an_BAR").forEach((bar) => 
+    { bar.style.setProperty("--bck_BAR", "0%"); bar.style.setProperty("--bck_BAR_2", "0%"); })
+
 }
 const BARs_langues = ( CIBLE_ID,val) => {  
   let i = 0;
   const boucle = ( ) => {
     if ( i < val ) { document.getElementById(CIBLE_ID).style.setProperty("--bck_BAR", i + "%"); }
-    if ( i < val ) { document.getElementById(CIBLE_ID).style.setProperty("--bck_BAR-2", (i-2) + "%"); }
+    if ( i < val ) { document.getElementById(CIBLE_ID).style.setProperty("--bck_BAR_2", (i-2) + "%"); }
     setTimeout(() => { i++; boucle();}, 20);
   }
   boucle();
@@ -71,14 +69,15 @@ const rebond_ = ( CIBLE_ID,TAB_refer,NBR_PiX,vitS) => {
 }
 
 (() => { 
-  document.getElementById("infosperso_BTN").style.animation = "1.5s linear forwards running opn_infosperso_BTN";
   document.getElementById("ThemeMenu_TXT").addEventListener('mouseenter', () => { ThemeMenu_BTN_.classList = "" }) ;              //OUVERTURE PASTILLES THEME
   document.getElementById("Entete_BOX").addEventListener('mouseleave', () => {  ThemeMenu_BTN_.classList = "_HIDE_" }) ;              //OUVERTURE PASTILLES THEME
   
   window.addEventListener('resize',(e) =>{  
     console.log(e.target.innerWidth);
-    if(!Bool_Compet_opn){ if(e.target.innerWidth > 955 ){ slid_Compet_OPN(); }}
-    if(Bool_Compet_opn){ if(e.target.innerWidth < 955 ){ slid_Compet_CLS(); }}  
+    dateShort();
+    
+    if(!Bool_Compet_opn){ if(e.target.innerWidth > 1000 ){ slid_Compet_OPN(); }}
+    if(Bool_Compet_opn){ if(e.target.innerWidth < 1000 ){ slid_Compet_CLS(); }}  
   })
 
   if( localStorage.theme ) { document.body.className = localStorage.theme; }  
@@ -92,7 +91,41 @@ const rebond_ = ( CIBLE_ID,TAB_refer,NBR_PiX,vitS) => {
 
 })()
 
-if(innerWIDTH > 955){ slid_Compet_OPN(); } else { slid_Compet_CLS(); }               //OUVERTURE PASTILLES THEME
+
+const dateShort = () =>{ 
+if(window.innerWidth > 440){ 
+  document.getElementById('exper_date_1').innerHTML = `  
+    <p>du <strong>17/06/2006</strong></p>
+    <p>au <strong>28/07/2021</strong></p>
+    <p><strong>Joigny</strong></p>
+  `    
+  document.getElementById('exper_date_2').innerHTML = `  
+  <p>du 17/06/2006</p>
+  <p>au 28/07/2021</p>
+  <p>Joigny</p>` 
+  
+  document.getElementById('exper_date_3').innerHTML = `  
+    <p>du 17/06/2006</p>
+    <p>au 28/07/2021</p>
+    <p>à Joigny</p>`}      
+else {
+  document.getElementById('exper_date_1').innerHTML = `  
+  <p><strong>06/06</strong></p>
+  <p><strong>07/21</strong></p>
+  <p><strong>Joigny</strong></p>
+    `
+  document.getElementById('exper_date_2').innerHTML = `  
+    <p>06/06</p>
+    <p>07/21</p>
+    <p>Joigny</p>`
+  document.getElementById('exper_date_3').innerHTML = `  
+    <p>06/06</p>
+    <p>07/21</p>
+    <p>Joigny</p>`} 
+}
+dateShort();
+
+if(window.innerWidth > 1000){ slid_Compet_OPN(); } else { slid_Compet_CLS(); }               //OUVERTURE PASTILLES THEME
 
 
 //=============================================================================================================================FONCTIONS**
